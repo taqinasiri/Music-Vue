b9a93b2790da63318e9e985d6bba4403ddf05827
Taqi ツ <mtaqinasiria@gmail.com>
2024-01-08 16:33:12 +0330

    Add routing and guard

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,13 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+      >
+        Music</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
@@ -13,12 +19,15 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" @click.prevent="userStore.signOut">Logout</a>
+              <a class="px-2 text-white" @click.prevent="signOut">Logout</a>
             </li>
           </template>
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+          </li>
         </ul>
       </div>
     </nav>
@@ -45,6 +54,13 @@ export default {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen;
       console.log(this.modalStore.isOpen);
+    },
+    signOut() {
+      this.userStore.signOut();
+
+      if (this.$routes.meta.requiresAuth) {
+        this.$route.push({ name: "home" });
+      }
     }
   }
 };
