88497fc8ec8e8ccc37b488938fb5f64216ff8b67
Taqi ツ <mtaqinasiria@gmail.com>
2024-01-09 20:25:37 +0330

    Add Upload songs

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,18 +1,22 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
   projectId: "",
   storageBucket: "",
-  appId: ""
+  appId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
-export { auth, db, usersCollection };
+const songsCollection = db.collection("songs");
+
+export { auth, db, usersCollection, songsCollection, storage };
