8be51bcad96ce31d47af6c93d76ca2def15449e7
Taqi ツ <mtaqinasiria@gmail.com>
2024-01-14 19:38:00 +0330

    Add PWA options

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import router from "./router";
 import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 let app;
 auth.onAuthStateChanged(() => {
