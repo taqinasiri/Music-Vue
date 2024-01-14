1753f506dfa3709c5445bc467abce0caf419705d
Taqi ツ <mtaqinasiria@gmail.com>
2024-01-14 20:07:09 +0330

    Add AutoRegister Global Components

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
 import { registerSW } from "virtual:pwa-register";
+import GlobalComponents from "./includes/_globals";
 
 registerSW({ immediate: true });
 
@@ -21,6 +22,7 @@ auth.onAuthStateChanged(() => {
     app.use(router);
     app.use(VeeValidatePlugin);
     app.use(i18n);
+    app.use(GlobalComponents);
     app.directive("icon", Icon);
   }
   app.mount("#app");
